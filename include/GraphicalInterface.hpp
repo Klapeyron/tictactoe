3e653ddfb14ac49fe78e2f83e4d1e12d900ffa0c
Klapeyron <t.m@o2.pl>
2015-05-28 11:19:09 +0200

    Definicja konstruktora interfejsu

--- include/GraphicalInterface.hpp
+++ include/GraphicalInterface.hpp
@@ -9,17 +9,17 @@
 using Image = cv::Mat;
 using ImageWindow = std::string;
 
+const std::string DEFAULT_WINDOW_NAME = "TicTacToe";
 const std::string TEMPLATE_FILES_DIRECTORY = "imageTemplates/";
 const std::vector<std::string> templateFileNames {"empty.png", "x.png", "o.png"};
 
 class GraphicalInterface :public MouseClickSubject {
-  const std::vector<Image> templateImages;
+  std::vector<Image> templateSymbolImages;
   Image windowImage;
   ImageWindow windowHandler;
   int gameSize;
  public:
-  // TODO: Dopisac argument do windowImage()
-  GraphicalInterface(const int size = 0) :gameSize(size), windowHandler("TicTacToe"), windowImage() {}
+  GraphicalInterface(const int size = 3);
   void updateTable(Symbol ** tablica, const int size);
   void showWinner(Symbol gracz);
   void clearWindow();
--- src/GraphicalInterface.cpp
+++ src/GraphicalInterface.cpp
@@ -1,5 +1,13 @@
 #include "GraphicalInterface.hpp"
 
+// TODO: Dopisac argument do windowImage()
+GraphicalInterface::GraphicalInterface(const int size)
+    :gameSize(size), windowHandler(DEFAULT_WINDOW_NAME), windowImage() {
+
+  for(auto const& fileName : templateFileNames)
+    templateSymbolImages.push_back( cv::imread(TEMPLATE_FILES_DIRECTORY+fileName,CV_LOAD_IMAGE_COLOR) );
+}
+
 void GraphicalInterface::clearWindow() {}
 
 void GraphicalInterface::showWinner(Symbol) {}
