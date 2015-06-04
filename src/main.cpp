bd7e5b19e5ee6903378aae1f2d87fb2a1fbe7a0f
Piotrek <209253@student.pwr.wroc.pl>
2015-06-04 14:50:01 +0200

    zabezpieczenie przed błędnym rozmiarem planszy

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,13 @@
 int main(int argc, char *argv[])
 {
   int x=3;
-  if(argc > 1)
+  if(argc > 1){
     x=atoi(argv[1]);
+    if(x <= 0){
+      std::cerr<<"Błąd! Rozmiar planszy musi być liczbą naturalną!\n";
+      exit(1);
+    }
+  }
   TicTacToe Gra(x);
   Gra.G.registerObserver(Gra);
   Gra.Game();
